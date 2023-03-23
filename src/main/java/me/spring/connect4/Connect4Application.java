cf398360182e71e589ec0506a394a4522db6bcfd
spring <springsquash93@gmail.com>
2023-03-23 03:55:51 -0400

    Import WebConfiguration

--- src/main/java/me/spring/connect4/Connect4Application.java
+++ src/main/java/me/spring/connect4/Connect4Application.java
@@ -1,11 +1,14 @@
 package me.spring.connect4;
 
+import me.spring.connect4.config.WebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "me.spring.connect4")
 @EnableJpaRepositories
+@Import(WebConfiguration.class)
 public class Connect4Application {
 
 	public static void main(String[] args) {
