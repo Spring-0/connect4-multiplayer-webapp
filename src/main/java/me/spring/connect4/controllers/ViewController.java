b4a4045303f916452d0fd54afb88237bd1e59661
spring <springsquash93@gmail.com>
2023-03-23 21:20:40 -0400

    Add register & login endpoint to ViewController

--- src/main/java/me/spring/connect4/controllers/ViewController.java
+++ src/main/java/me/spring/connect4/controllers/ViewController.java
@@ -1,16 +1,22 @@
 package me.spring.connect4.controllers;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class ViewController {
 
 
-    @GetMapping("")
-    public String index(){
-        return "index";
+    @GetMapping("/register")
+    public String register(){
+        return "register.html";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login.html";
     }
 
 
