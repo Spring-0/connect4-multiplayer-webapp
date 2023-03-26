8da9ddb29c9a25178ff427607c68688a5578f1fc
spring <springsquash93@gmail.com>
2023-03-26 01:06:38 -0400

    Add GameState Data Transfer Object (dto)

---
+++ src/main/java/me/spring/connect4/controllers/dto/GameState.java
@@ -0,0 +1,24 @@
+package me.spring.connect4.controllers.dto;
+
+import me.spring.connect4.models.player.Player;
+
+public class GameState {
+
+    private String gameID;
+    private boolean winner = false;
+    private int pieceRow;
+    private int pieceCol;
+    private Player lastPlayed;
+
+    public GameState(String gameID, Player lastPlayed, int pieceRow, int pieceCol, boolean winner) {
+        this.gameID = gameID;
+        this.lastPlayed = lastPlayed;
+        this.pieceCol = pieceCol;
+        this.pieceRow = pieceRow;
+        this.winner = winner;
+    }
+
+    public String getGameID() {
+        return this.gameID;
+    }
+}
