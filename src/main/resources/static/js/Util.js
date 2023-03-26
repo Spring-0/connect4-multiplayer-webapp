5ad0a77cf834bbef69da7ea7feb7ab7ff5255045
spring <springsquash93@gmail.com>
2023-03-26 12:59:52 -0400

    Refactor postText and postJson functions to Util.js

--- src/main/resources/static/js/Util.js
+++ src/main/resources/static/js/Util.js
@@ -43,3 +43,50 @@ function updateNames(response){
     document.getElementById("gameID").innerHTML = response.gameID;
 }
 
+/**
+ * Helper Function to send post request with raw text body
+ *
+ * @param endpoint
+ * @param body
+ * @returns {Promise<any>}
+ */
+function postText(endpoint, body) {
+    return fetch(endpoint, {
+        method: "POST",
+        body: body
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error " + response.status);
+            }
+            console.log(response.status)
+            return response.json();
+        })
+        .then(data => {
+            console.log("Response received:", data);
+            return data;
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
+
+/**
+ * Helper Function to post JSON data to endpoint
+ *
+ * @param endpoint
+ * @param data
+ * @returns {Promise<any>}
+ */
+function postJson(endpoint, data) {
+    return fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .catch(error => console.error(error));
+}
+
