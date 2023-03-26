4105ce0beecf0e11f0a27e4b6bc085684ac12e9f
spring <springsquash93@gmail.com>
2023-03-26 01:17:19 -0400

    Add JS to add Click Event Listeners for columns in game board table.

---
+++ src/main/resources/static/js/GameBoard.js
@@ -0,0 +1,24 @@
+
+// Get all the columns from game board
+const columns = document.querySelectorAll('#game-board td');
+
+// Add click listener for each col
+columns.forEach((column) => {
+    column.addEventListener('click', () => {
+        // Get the index of the clicked column
+        const colIndex = Array.from(column.parentNode.children).indexOf(column);
+        console.log(`Clicked column index: ${colIndex}`);
+
+        const data = {
+            gameID: document.getElementById("gameID").innerHTML,
+            col: colIndex,
+            playerID: getPlayerIDFromCookie()
+        }
+
+        postJson("/game/playgame", data)
+            .then(response =>{
+                // Do nothing
+            });
+
+    });
+});
